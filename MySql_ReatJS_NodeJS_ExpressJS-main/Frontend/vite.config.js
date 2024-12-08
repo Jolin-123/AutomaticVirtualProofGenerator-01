0a141ab31fa05a3a4d4e213b2c1571bd16c82efa
Jolin-123 <milklinschool@gmail.com>
2024-12-08 01:08:16 -0800

    vite.config change

--- MySql_ReatJS_NodeJS_ExpressJS-main/Frontend/vite.config.js
+++ MySql_ReatJS_NodeJS_ExpressJS-main/Frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
-  // ...
-  server:{
-    host:true,
-    port:80,
-  },
-});
+  plugins: [react()],
+})
+
+// export default defineConfig({
+//   // ...
+//   server:{
+//     host:true,
+//     port:80,
+//   },
+// });
